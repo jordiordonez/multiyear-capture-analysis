132de06577233b3409cf0a911f2df7f6ae34b7f3
Jordi Ordoñez Adellach <jordiordonezadellach@MacBook-Air-de-mac.local>
2025-05-01 10:56:47 +0200

    Sorteig total

--- app_sorteig.py
+++ app_sorteig.py
@@ -128,14 +128,14 @@ def assignar_captura_csv(
 st.title("App Sorteig Pla de Caça")
 # Instruccions d'ús en català
 st.markdown("""
-    **Instruccions d'ús:**
+    ## Instruccions d'ús:
 
     1. Seleccioneu l'espècie i la unitat de gestió.
     2. Pugeu el fitxer CSV de sol·licitants.
     3. Si no és Isard + TCC, afegiu un o més Tipus de captura en l'ordre que es sortejaran :
-    - Clic a "Afegeix Tipus", 
-    - seleccioneu un o diversos valors 
-    - indiqueu el nombre de captures.
+        - Clic a "Afegeix Tipus", 
+        - seleccioneu un o diversos valors 
+        - indiqueu el nombre de captures.
     4. Opcional: introduïu una llavor per a reproduir el mateix sorteig.
     5. Feu clic a "Executar sorteig" per veure els resultats i descarregar el CSV.
     """)
