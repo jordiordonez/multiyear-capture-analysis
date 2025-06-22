d57135905198ce6d0a42324392f04abdf029bac6
Jordi Ordoñez Adellach <jordiordonezadellach@MacBook-Air-de-mac.local>
2025-06-22 09:23:23 +0200

    treball ultim any sc

--- app_sorteig.py
+++ app_sorteig.py
@@ -190,12 +190,12 @@ def assignar_captura_csv(
     )
     # Calcular nova prioritat i anys sense captura
     df["nova_prioritat"] = df.apply(
-        lambda row: 4 if row["Adjudicats_acumulats"] > 0 else row["Prioritat"],
-        axis=1
+        lambda row: 4 if row["Nou_Resultat_sorteigs_mateixa_sps"] > 0 else row["Prioritat"],
+    axis=1
     )
-    df["nova_prioritat Any següent"] = df["Adjudicats_acumulats"].apply(lambda x: 4 if x > 0 else 2)
+    df["nova_prioritat Any següent"] = df["Nou_Resultat_sorteigs_mateixa_sps"].apply(lambda x: 4 if x > 0 else 2)
     df["nou_anys_sense_captura"] = df.apply(
-        lambda r: 0 if r["Adjudicats_acumulats"] > 0 else r["anys_sense_captura"] + 1, axis=1
+        lambda r: 0 if r["Nou_Resultat_sorteigs_mateixa_sps"] > 0 else r["anys_sense_captura"] + 1, axis=1
     )
     if "Adjudicats_acumulats" in df.columns:
         df.drop(columns=["Adjudicats_acumulats"], inplace=True)
@@ -294,11 +294,12 @@ def assignar_captura_parroquial_csv(
     )
    # Calcular nova prioritat i anys sense captura
     df["nova_prioritat"] = df.apply(
-        lambda row: 4 if row["Adjudicats_acumulats"] > 0 else row["Prioritat"]
+        lambda row: 4 if row["Nou_Resultat_sorteigs_mateixa_sps"] > 0 else row["Prioritat"],
+    axis=1
     )
-    df["nova_prioritat Any següent"] = df["Adjudicats_acumulats"].apply(lambda x: 4 if x > 0 else 2)
+    df["nova_prioritat Any següent"] = df["Nou_Resultat_sorteigs_mateixa_sps"].apply(lambda x: 4 if x > 0 else 2)
     df["nou_anys_sense_captura"] = df.apply(
-        lambda r: 0 if r["Adjudicats_acumulats"] > 0 else r["anys_sense_captura"] + 1, axis=1
+        lambda r: 0 if r["Nou_Resultat_sorteigs_mateixa_sps"] > 0 else r["anys_sense_captura"] + 1, axis=1
     )
     if "Adjudicats_acumulats" in df.columns:
         df.drop(columns=["Adjudicats_acumulats"], inplace=True)
